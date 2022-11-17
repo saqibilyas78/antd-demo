feeac227613b191dba68c911695ef23045016094
Rana Saqib <rs@adeogroup.co.uk>
2022-11-17 14:58:33 +0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
 import './App.css';
-import { Space, Table, Button } from 'antd';
-import React, { useEffect } from 'react';
+import { Space, Table, Button, Modal ,Checkbox, Form, Input, Select, Switch} from 'antd';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { add, remove } from './account'
-
+import { add } from './account'
+  
 function App() {
 
   const dispatch = useDispatch();
   const accounts = useSelector((state) => state.account.accounts);
   const count = useSelector((state) => state.account.count);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm();
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [parent , set_parent] = useState(0);
 
-  const add_account = () => {
-
-    const obj = {
-      key: count,
-      action : '',
-      name: 'John Brown',
-      type: 32,
-      category: 'New York No. 1 Lake Park',
-      status: 'New York No. 1 Lake Park',
-      note: 'New York No. 1 Lake Park',
-    }
-
-    dispatch(add(obj))
+  const add_account = (account) => {
+    dispatch(add(account))
   }
+
+  const formItemLayout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 12 },
+  };
+  
+   const showModal = (parent) => {
+    set_parent(parent)
+    setIsModalOpen(true);
+  };
+
+  const handleOk = async () => {
+    try {
+      const values = await form.validateFields();
+      let status = values.status == true ? 'Yes' : 'No'
+      const account = {
+        key: count,
+        action : '',
+        name: values.name,
+        type: values.type,
+        category: values.category,
+        status: status,
+        note: values.note,
+        parent : parent,
+        children : []
+      }
+      console.log('Success:', account);
+      add_account(account);
+      setIsModalOpen(false);
+      form.resetFields();
+    } catch (errorInfo) {
+      console.log('Failed:', errorInfo);
+    }
+    
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const columns = [
     {
@@ -35,9 +67,9 @@ function App() {
     {
       title: 'Action',
       key: 'action',
-      render: (_, record) => (
+      render: (value, record) => (
         <Space size="middle">
-          <a onClick={() => add_account()}>+Add</a>
+          <a href='#' onClick={() => showModal(record.key)}>+Add</a>
         </Space>
       ),
     },
@@ -66,10 +98,108 @@ function App() {
   const btnStyle = {
     float: 'right',
   };
+
+  const types = [
+    { label: 'Debit', value: 'Debit' },
+    { label: 'Credit', value: 'Credit' },
+  ];
+
+  const category = [
+    { label: 'Resource', value: 'Resource' },
+    { label: 'Money', value: 'Money' },
+  ];
+
+
+  const onSelectChange = (newSelectedRowKeys) => {
+    console.log('selectedRowKeys changed: ', newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+    selections: [
+      Table.SELECTION_ALL,
+      Table.SELECTION_INVERT,
+      Table.SELECTION_NONE,
+      {
+        key: 'odd',
+        text: 'Select Odd Row',
+        onSelect: (changableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return false;
+            }
+            return true;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+      {
+        key: 'even',
+        text: 'Select Even Row',
+        onSelect: (changableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return true;
+            }
+            return false;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+    ],
+  };
+
+  useEffect(() => {
+  }, [form]);
+
   return (
     <div className="App">
-        {(accounts.length == 0 ? <Button type="primary" onClick={() => add_account()} style={btnStyle}>Add First Account</Button> : (''))}
-        <Table columns={columns} dataSource={accounts} />
+        <Button type="primary" onClick={() => showModal(0)} style={btnStyle}>Add Account</Button>
+        <Table rowSelection={rowSelection} columns={columns} dataSource={accounts} />
+
+        <Modal title="Add Account" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <Form form={form} name="dynamic_rule">
+          <Form.Item
+            {...formItemLayout}
+            name="name"
+            label="Account Name"
+            rules={[
+              {
+                required: true,
+                message: 'Please input account name',
+              },
+            ]}
+          >
+            <Input placeholder="Please input account name" />
+          </Form.Item>
+          <Form.Item {...formItemLayout} name="type" label="Account Type" rules={[{ required: true, message: 'Please select account type' }]}>
+            <Select options={types} />
+          </Form.Item>
+          <Form.Item {...formItemLayout} name="category" label="Account Category" rules={[{ required: true, message: 'Please select account category' }]}>
+            <Select options={category} />
+          </Form.Item>
+          <Form.Item {...formItemLayout} label="Use Status" name="status" valuePropName="status">
+            <Switch />
+          </Form.Item>
+          <Form.Item
+            {...formItemLayout}
+            name="note"
+            label="Note"
+            rules={[
+              {
+                required: true,
+                message: 'Please input note',
+              },
+            ]}
+          >
+            <Input placeholder="Please input note" />
+          </Form.Item>
+        </Form>
+        
+      </Modal>
     </div>
   );
 }
--- src/account.js
+++ src/account.js
@@ -1,5 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const findNestedAccountObject = (array, key) => {
+  let account;
+
+  for (const obj of array) {
+    if (obj.key === key) {
+      account = obj;
+    } else if (obj.children !== undefined) {
+      account = findNestedAccountObject(obj.children, key);
+    }
+    if (account !== undefined) {
+      break;
+    }
+  }
+
+  return account;
+}
+
 export const accountSlice = createSlice({
   name: 'account',
   initialState: {
@@ -8,18 +25,20 @@ export const accountSlice = createSlice({
   },
   reducers: {
     add: (state, action) => {
-        state.accounts.push({ ...action.payload});
+        // const itemExists = state.accounts.find((item) => item.key === action.payload.parent);
+        const itemExists = findNestedAccountObject(state.accounts, action.payload.parent)
+        console.log(itemExists);
+        if(itemExists){
+          itemExists.children.push({ ...action.payload});
+        }else{
+          state.accounts.push({ ...action.payload});
+        }
         state.count += 1;
-    },
-    remove: (state, action) => {
-      const index = state.accounts.findIndex((item) => item.key === action.payload);
-      state.accounts.splice(index, 1);
-      state.count -= 1;
     },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { add, remove } = accountSlice.actions
+export const { add } = accountSlice.actions
 
 export default accountSlice.reducer
